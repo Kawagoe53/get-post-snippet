29c32d316ac620669424f94da4da15c4cf7e32b3
Kawagoe53 <y.kawagoe1231@gmail.com>
2023-08-05 00:15:45 +0900

    refactor:問題1 コードの修正

--- src/public/question/q1/post.php
+++ src/public/question/q1/post.php
@@ -2,6 +2,31 @@
 $title = filter_input(INPUT_POST, 'title');
 $email = filter_input(INPUT_POST, 'email');
 $content = filter_input(INPUT_POST, 'content');
+
+$errors = [];
+if(empty($title)||empty($email)||empty($content)){
+  $errors[] = '「タイトル」「email」「お問い合わせ内容」のどれかが記入されていません!';
+}
+
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO(
+  "mysql:host=mysql; dbname=contactform; charset=utf8", 
+  $dbUserName, 
+  $dbPassword
+); 
+
+$stmt = $pdo->prepare("INSERT INTO contacts (
+	title, email, content
+) VALUES (
+	:title, :email, :content
+)");
+
+$stmt->bindParam( ':title', $title, PDO::PARAM_STR);
+$stmt->bindParam( ':email', $email, PDO::PARAM_STR);
+$stmt->bindParam( ':content', $content, PDO::PARAM_STR);
+$res = $stmt->execute();
+
 ?>
 
 <body>
